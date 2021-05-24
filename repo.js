2a358cedae96620b026c73a955ddfcc497663abb
shirokurakana <admin@thdog.moe>
2021-05-24 23:21:51 +0800

    add repo.js

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "admin@thdog.moe",
+    "id": "shirokura",
+    "patches": {
+        "th18_card_chs": "Human readable ability card Chinese translate by THBWiki"
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/shirokura/"
+    ],
+    "title": "shirokura's Repository"
+}
