b226e6e1040a3e9bb208acc31e15d265b5ac0665
shirokurakana <admin@thdog.moe>
2021-05-24 21:54:23 +0800

    add patch.js

---
+++ patch.js
@@ -0,0 +1,10 @@
+{
+    "dependencies": [
+        "thpatch/lang_zh-hans"
+    ],
+    "id": "th18_card_chs",
+    "servers": [
+        "https://raw.githubusercontent.com/shirokurakana/th18_card_chs/master/"
+    ],
+    "title": "Human readable ability card Chinese translate by THBWiki for thcrap users"
+}
